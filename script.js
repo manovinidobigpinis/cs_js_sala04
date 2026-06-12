cb4485d46945c18749e2db18569afd346bc43ba9
Vinicius Tomazoni Renaud <viniestudamuitomesmo@gmail.com>
2026-06-12 08:01:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function media() {
+
+    var num1 = parseFloat.(document.getElementById('n1').value)
+    var num2 = parseFloat.(document.getElementById('n2').value)
+    var num3 = parseFloat.(document.getElementById('n3').value)
+
+    var resposta = document.getElementById('resposta')
+
+    console.log(media)
+}
